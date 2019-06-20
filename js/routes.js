52789ffee6306cdd06186f921eb0c80fc6867f2f
Tal Maoz <tal.maoz.work@gmail.com>
2019-06-20 16:05:52 +0300

    transition from books to notes - WIP 11

--- js/routes.js
+++ js/routes.js
@@ -1,4 +1,4 @@
-import homepageCmp from './pages/homepage.cmp.js';
+import homepageCmp from './pages/home-page.cmp.js';
 import aboutCmp from './pages/about-page.cmp.js';
 import notesApp from './pages/notes-app.cmp.js';
 // import noteDetails from './pages/book-details.cmp.js';
