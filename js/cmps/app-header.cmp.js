e96a1d4aa4bad00750bb57bcb17351199ce4d67c
Tal Maoz <tal.maoz.work@gmail.com>
2019-06-20 14:46:11 +0300

    WIP WIP WIP

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -6,26 +6,11 @@ export default {
                 <h1>TalSus</h1>
             </div>
             <div class="flex-column-container">
-                <button class="apps-menu-btn">
-                </button>
-                <nav 
-                    v-if="appListOpen">
-                    <router-link exact to="/">Home</router-link>
-                    <router-link to="/about" >About</router-link>
-                    <router-link to="/book"  >Books</router-link>
-                </nav>
+                <router-link
+                    class="apps-menu-btn"
+                    exact to="/">
+                </router-link>
             </div>     
         </div>    
     `,
-    data() {
-        return {
-            appListOpen : false,
-        }
-    },
-    created() {
-        // created()
-    },
-    methods: {
-        // methods funcs
-    }
 }
--- js/pages/homepage.cmp.js
+++ js/pages/homepage.cmp.js
@@ -2,6 +2,9 @@ export default {
     template: `
     <section class="home-page">
         <h1>Home Sweet Home</h1>
+        <router-link exact to="/book">Notes</router-link>
+        <router-link exact to="/book">EmailSus</router-link>
+        <router-link exact to="/about">About</router-link>
     </section>
     `
 }
