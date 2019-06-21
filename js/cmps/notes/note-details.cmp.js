9585b1a7473b9a741f4e11d5bfe71e06b7d47001
Tal Maoz <tal.maoz.work@gmail.com>
2019-06-21 14:43:59 +0300

    WIP 4

--- js/cmps/notes/note-details.cmp.js
+++ js/cmps/notes/note-details.cmp.js
@@ -3,20 +3,37 @@ export default {
     template: `
         <div class="note-details">
             <button @click="emitBackToList">Back To Notes List</button>
-            <h3>{{note.title}}</h3>
-            <h4>{{}} Note content will appear here</h4>
-            <img v-bind:title="note.title" v-bind:src="note.thumbnail" />
+            <h3>
+                <input 
+                    v-model="editedNote.title"
+                    v-bind:placeholder="titlePlaceholder"
+                />
+            </h3>
+            <img v-if="note.thumbnail" v-bind:title="note.title" v-bind:src="note.thumbnail">
+            <h4  v-if="note.txt">{{note.txt}}</h4>
+            <ul v-if="note.checkList"> 
+                <li
+                    v-bind:key="checkItem"
+                    v-for="checkItem in note.checkList">
+                    - {{checkItem}}
+                </li>
+            </ul>
         </div>
     `,
     computed: {
-        someFunc() {
-            // return computed value
+        titlePlaceholder() {
+            return (this.note.title)? '' : 'Title...'
         },
     },
     methods: {
         emitBackToList () {
             this.$emit('back-to-list', '')
         },
+    },
+    data () {
+        return {
+            editedNote : this.note,
+        }
     },
 }
 
