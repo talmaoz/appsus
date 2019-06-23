2872c5a4260fb85dc912a1d7c9bd69210ab9e125
Tal Maoz <tal.maoz.work@gmail.com>
2019-06-23 18:35:00 +0300

    Mile Stone

--- js/cmps/notes/notes-new.cmp.js
+++ js/cmps/notes/notes-new.cmp.js
@@ -3,15 +3,21 @@ import notesService from '../../services/notes.service.js'
 export default {
     template: `
         <section class="notes-new">
+            <div class="flex-column-container">
+                <p>New note: </p>
+            </div>
             <button 
+                title="New plain text note."
                 v-on:click="emitNewNote('txt-note')"
                 class="new-txt-note-btn">
             </button>
-            <button 
+            <button
+                title="New check-list note." 
                 v-on:click="emitNewNote('checklist-note')"
                 class="new-checklist-note-btn">
             </button>
             <button 
+                title="New image note."
                 v-on:click="emitNewNote('img-note')"
                 class="new-img-note-btn">
                     IMG
